546d190d9228113e0b1948ef65cd4cb0f4a6e35b
AndresSC25 <aseguracalderon26@gmail.com>
2024-07-16 16:07:37 -0600

    Ingreso de crud y daos, recordar cambiar el connectionString

---
+++ GymBackend/Gym/DataAccess/DAOs/SqlOperation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAccess.DAOs;
+
+public class SqlOperation
+
+{
+    public SqlOperation()
+    {
+        Parameters = new List<SqlParameter>();
+    }
+
+    public string ProcedureName { get; set; }
+    public List<SqlParameter> Parameters { get; set; }
+
+    public void AddStringParam(string paramName, string paramValue)
+    {
+        Parameters.Add(new SqlParameter(paramName, paramValue));
+    }
+
+    public void AddIntParam(string paramName, int paramValue)
+    {
+        Parameters.Add(new SqlParameter(paramName, paramValue));
+    }
+
+    public void AddDateTimeParam(string paramName, DateTime paramValue)
+    {
+        Parameters.Add(new SqlParameter(paramName, paramValue));
+    }
+
+    public void AddDoubleParam(string paramName, double paramValue)
+    {
+        Parameters.Add(new SqlParameter(paramName, paramValue));
+    }
+}
