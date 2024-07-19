560c372e49a832e4126b4551312e76f3dfd4974a
Loyd Garmany <141684401+loydgarmany@users.noreply.github.com>
2024-07-18 22:04:39 -0600

    Manager and Controller creation for user
    Created with all of the values set.

--- GymBackend/Gym/WebAPI/Controllers/UserController.cs
+++ GymBackend/Gym/WebAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using CoreApp;
+using DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers;
@@ -7,7 +8,84 @@ namespace WebAPI.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
+
+    #region POSTS
+
+    [HttpPost]
+    [Route("Create")]
+    public ActionResult Create(User user)
+    {
+        try
+        {
+            var um = new UserManager();
+            um.Create(user);
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region PUT
+
+    [HttpPut]
+    [Route("Update")]
+    public ActionResult Update(User user)
+    {
+        try
+        {
+            var um = new UserManager();
+            um.Update(user);
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region DELETE
+
+    [HttpDelete]
+    [Route("Delete")]
+    public ActionResult Delete(User user)
+    {
+        try
+        {
+            var um = new UserManager();
+            um.Delete(user);
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    #endregion
+
     #region GETS
+
+    [HttpGet]
+    [Route("RetrieveAll")]
+    public ActionResult RetrieveAll()
+    {
+        try
+        {
+            var um = new UserManager();
+            return Ok(um.RetrieveAll());
+        }
+        catch (Exception ex)
+        {
+            // 500 es internal server error
+            return StatusCode(500, ex.Message);
+        }
+    }
 
     [HttpGet]
     [Route("RetrieveById")]
@@ -20,6 +98,7 @@ public class UserController : ControllerBase
         }
         catch (Exception ex)
         {
+            // 500 es internal server error
             return StatusCode(500, ex.Message);
         }
     }
