5260276cd9bd157a9012b62ba0cbd8da6ccde4af
Loyd Garmany <141684401+loydgarmany@users.noreply.github.com>
2024-07-18 21:55:00 -0600

    Membership, Discount and User Crud Factory Creation
    Crud Factories created, with all of their subsequent queries.

---
+++ GymBackend/Gym/DataAccess/CRUD/MembershipCrudFactory.cs
@@ -0,0 +1,104 @@
+﻿using DataAccess.DAOs;
+using DTOs;
+
+namespace DataAccess.CRUD
+{
+    public class MembershipCrudFactory : CrudFactory
+    {
+        public MembershipCrudFactory()
+        {
+            _sqlDao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseDTO baseDto)
+        {
+            var membership = baseDto as Membership;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "CRE_MEMBERSHIP_PR";
+            sqlOperation.AddStringParam("P_Type", membership.Type);
+            sqlOperation.AddIntParam("P_AmountClassesAllowed", membership.AmountClassesAllowed);
+            sqlOperation.AddDoubleParam("P_MonthlyCost", membership.MonthlyCost);
+            sqlOperation.AddDateTimeParam("P_Created", membership.Created);
+
+            _sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override void Delete(BaseDTO baseDto)
+        {
+            var membership = baseDto as Membership;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "DEL_MEMBERSHIP_PR";
+            sqlOperation.AddIntParam("P_Id", membership.Id);
+
+            _sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override T Retrieve<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            var lstMemberships = new List<T>();
+            var sqlOperation = new SqlOperation() { ProcedureName = "RET_ALL_MEMBERSHIPS_PR" };
+            var lstResults = _sqlDao.ExecuteQueryProcedure(sqlOperation);
+            if (lstResults.Count > 0)
+            {
+                foreach (var row in lstResults)
+                {
+                    var membership = BuildMembership(row);
+                    lstMemberships.Add((T)Convert.ChangeType(membership, typeof(T)));
+                }
+            }
+            return lstMemberships;
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            var sqlOperation = new SqlOperation() { ProcedureName = "RET_MEMBERSHIP_BY_ID_PR" };
+            sqlOperation.AddIntParam("P_Id", id);
+
+            var lstResults = _sqlDao.ExecuteQueryProcedure(sqlOperation);
+            if (lstResults.Count > 0)
+            {
+                var row = lstResults[0];
+                var retMembership = (T)Convert.ChangeType(BuildMembership(row), typeof(T));
+                return retMembership;
+            }
+            return default;
+        }
+
+        public override void Update(BaseDTO baseDto)
+        {
+            var membership = baseDto as Membership;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "UPD_MEMBERSHIP_PR";
+            sqlOperation.AddIntParam("P_Id", membership.Id);
+            sqlOperation.AddStringParam("P_Type", membership.Type);
+            sqlOperation.AddIntParam("P_AmountClassesAllowed", membership.AmountClassesAllowed);
+            sqlOperation.AddDoubleParam("P_MonthlyCost", membership.MonthlyCost);
+            sqlOperation.AddDateTimeParam("P_Created", membership.Created);
+
+            _sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        #region Funciones extras
+        private Membership BuildMembership(Dictionary<string, object> row)
+        {
+            var membershipToReturn = new Membership()
+            {
+                Id = (int)row["id"],
+                Type = (string)row["type"],
+                AmountClassesAllowed = (int)row["amount_classes_allowed"],
+                MonthlyCost = (double)row["monthly_cost"],
+                Created = (DateTime)row["created"]
+            };
+            return membershipToReturn;
+        }
+        #endregion
+    }
+}
