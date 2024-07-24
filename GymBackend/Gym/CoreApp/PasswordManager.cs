4f18b2a540bdbcee70bf1064cd62d4e245b4e295
Loyd Garmany <141684401+loydgarmany@users.noreply.github.com>
2024-07-23 19:12:09 -0600

    Password CRUDs
    Created password cruds

---
+++ GymBackend/Gym/CoreApp/PasswordManager.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.CRUD;
+using DTOs;
+
+namespace CoreApp
+{
+    public class PasswordManager
+    {
+        public void Create(Password password)
+        {
+            var pCrud = new PasswordCrudFactory();
+            // Validaciones adicionales
+            pCrud.Create(password);
+        }
+
+        public void Update(Password password)
+        {
+            var pCrud = new PasswordCrudFactory();
+            pCrud.Update(password);
+        }
+
+        public void Delete(Password password)
+        {
+            var pCrud = new PasswordCrudFactory();
+            pCrud.Delete(password);
+        }
+
+        public List<Password> RetrieveAll()
+        {
+            var pCrud = new PasswordCrudFactory();
+            return pCrud.RetrieveAll<Password>();
+        }
+
+        public Password RetrieveById(int id)
+        {
+            var pCrud = new PasswordCrudFactory();
+            return pCrud.RetrieveById<Password>(id);
+        }
+
+        // Aquí irían las validaciones
+
+        #region Validations
+
+        #endregion
+    }
+}
---
+++ GymBackend/Gym/WebAPI/Controllers/PasswordController.cs
@@ -0,0 +1,110 @@
+﻿using CoreApp;
+using DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        // CREATE --> POST
+        // RETRIEVE --> GET
+        // UPDATE --> PUT
+        // DELETE --> DELETE
+
+        #region POSTS
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult Create(Password password)
+        {
+            try
+            {
+                var pm = new PasswordManager();
+                pm.Create(password);
+                return Ok(password);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region PUT
+
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult Update(Password password)
+        {
+            try
+            {
+                var pm = new PasswordManager();
+                pm.Update(password);
+                return Ok(password);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region DELETE
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(Password password)
+        {
+            try
+            {
+                var pm = new PasswordManager();
+                pm.Delete(password);
+                return Ok(password);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region GETS
+
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
+        {
+            try
+            {
+                var pm = new PasswordManager();
+                return Ok(pm.RetrieveAll());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveById")]
+        public ActionResult RetrieveById(int id)
+        {
+            try
+            {
+                var pm = new PasswordManager();
+                return Ok(pm.RetrieveById(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
