515d393b2a2f7bb9941563df31aa4d30ac50df91
luischino05 <alvaradoarias05@gmail.com>
2024-07-26 21:07:03 -0600

    CRUD MEASURES COMPLETED

---
+++ GymBackend/Gym/WebAPI/Controllers/MeasureController.cs
@@ -0,0 +1,96 @@
+﻿using CoreApp;
+using DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeasuresController : Controller
+    {
+        #region POSTS
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult Create(Measures measure)
+        {
+            try
+            {
+                var mm = new MeasuresManager();
+                mm.Create(measure);
+                return Ok(measure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var mm = new MeasuresManager();
+                var measure = mm.RetrieveById(id);
+                if (measure == null)
+                {
+                    return NotFound();
+                }
+                mm.Delete(measure);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region GETS
+
+        [HttpGet]
+        [Route("RetrieveById")]
+        public ActionResult RetrieveById(int id)
+        {
+            try
+            {
+                var mm = new MeasuresManager();
+                var measure = mm.RetrieveById(id);
+                if (measure == null)
+                {
+                    return NotFound();
+                }
+                return Ok(measure);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
+        {
+            try
+            {
+                var mm = new MeasuresManager();
+                return Ok(mm.RetrieveAll());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
