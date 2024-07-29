293561162acdb9bf6571a973614dd1c6a44dddb0
AndresSC25 <aseguracalderon26@gmail.com>
2024-07-28 19:59:30 -0600

    Se creo la logica para cambiar la contraseña. y se crearon validaciones para diferentes cosas aparte del envio de notificacion por correo del cambio de la contraseña.

--- GymBackend/Gym/CoreApp/SendGridEmail.cs
+++ GymBackend/Gym/CoreApp/SendGridEmail.cs
@@ -22,5 +22,18 @@ public class SendGridEmail
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         var response = await client.SendEmailAsync(msg);
     }
+
+    //Se quita el async y el await por problemas con el codigo.
+    public void SendEmailAsyncPasswordChanges(string toEmail)
+    {
+        var client = new SendGridClient(_apiKey);
+        var from = new EmailAddress("siluetaclubfitness@gmail.com", "Silueta Club Fitness");
+        var to = new EmailAddress(toEmail);
+        var subject = "Su clave ha sido cambiada";
+        var plainTextContent = "Nueva clave registrada en Silueta Club Fitness";
+        var htmlContent = "<h3>Se ha cambiado tu clave, si no fuiste tú por favor verifica.</h3>";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response =  client.SendEmailAsync(msg);
+    }
 }
 
