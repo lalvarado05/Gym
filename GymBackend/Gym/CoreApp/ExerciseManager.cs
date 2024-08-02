84237c938fb461247504af10a7b790ae5aeafcce
csuncin <csuncinm@ucenfotec.ac.cr>
2024-08-01 19:53:50 -0600

    Validations added and bugs fixed

--- GymBackend/Gym/CoreApp/ExerciseManager.cs
+++ GymBackend/Gym/CoreApp/ExerciseManager.cs
@@ -15,6 +15,7 @@ public class ExerciseManager
     public void Update(Exercise exercise)
     {
         var eCrud = new ExerciseCrudFactory();
+        ExerciseTypeValidation(exercise);
         eCrud.Update(exercise);
     }
 
@@ -64,7 +65,19 @@ public class ExerciseManager
             }
 
             exercise.Sets = 0;
-            exercise.Reps = 0;
+            exercise.Duration = 0;
+        }
+
+        if (string.IsNullOrWhiteSpace(exercise.Name) ||
+            string.IsNullOrWhiteSpace(exercise.Type) ||
+            exercise.EquipmentId <= 0 ||
+            string.IsNullOrWhiteSpace(exercise.EquipmentName) ||
+            exercise.Sets < 0 ||
+            exercise.Reps < 0 ||
+            exercise.Weight < 0 ||
+            exercise.Duration < 0)
+        {
+            throw new Exception("Faltan campos requeridos");
         }
     }
 
